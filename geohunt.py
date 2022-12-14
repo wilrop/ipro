Bugfix the target vector computation which depends on the side of entry for the line

geohunt.py
@@ -9,13 +9,6 @@ def inner_loop(momdp, target):
     return vec
 
 
-def get_target_vec(patch):
-    slope = patch.midpoint[1] / patch.midpoint[1]
-    y = slope * patch.max_x
-    target = np.array([patch.max_x, y])
-    return target
-
-
 def get_edge_points():
     return np.array([5, 1]), np.array([1, 5])
 
@@ -30,7 +23,7 @@ def geohunt2d(momdp, tolerance=0.1):
     while patches_queue:
         patch = patches_queue.pop()
 
-        target = get_target_vec(patch)
+        target = patch.get_intersection_point()
         pareto_optimal_vec = inner_loop(momdp, target)
         if pareto_optimal_vec not in [patch.point1, patch.point2]:  # Check that a new Pareto optimal point was found.
             new_patch1, new_patch2 = patch.split(pareto_optimal_vec)

patch2d.py
@@ -7,11 +7,21 @@ class Patch2d:
     """
 
     def __init__(self, point1, point2):
-        self.point1 = point1
-        self.point2 = point2
+        self.top_left, self.top_right, self.bot_left, self.bot_right = None, None, None, None
+        self.init_rectangle(point1, point2)
         self.midpoint = (point1 + point2) / 2
-        self.max_x = max(point1[0], point2[0])
-        self.area = np.prod(np.abs(np.array(self.point1) - np.array(self.point2)))
+        self.area = np.prod(np.abs(np.array(point1) - np.array(point2)))
+
+    def init_rectangle(self, point1, point2):
+        if point1[0] > point2[0]:
+            self.bot_right = point1
+            self.top_left = point2
+        else:
+            self.bot_right = point2
+            self.top_left = point1
+
+        self.top_right = np.array([self.bot_right[0], self.top_left[1]])
+        self.bot_left = np.array([self.top_left[0], self.bot_right[1]])
 
     def split(self, point):
         """Split a patch in two.
@@ -22,6 +32,29 @@ class Patch2d:
         Returns:
             Patch2d, Patch2d: Two new patches.
         """
-        patch1 = Patch2d(self.point1, point)
-        patch2 = Patch2d(self.point2, point)
+        patch1 = Patch2d(self.top_left, point)
+        patch2 = Patch2d(self.bot_right, point)
         return patch1, patch2
+
+    def get_intersection_point(self, start=(0, 0)):
+        """Get the furthest intersection point of a line through the midpoint with the patch.
+
+        Args:
+            start (array_like, optional): The start point for the line. By default, this is the origin.
+
+        Returns:
+            ndarray: The intersection point.
+        """
+        start = np.array(start)
+        diffs = self.midpoint - start
+        slope = diffs[1] / diffs[0]
+        b = start[1] - slope * start[0]
+
+        y_at_bot_left = slope * self.bot_left[0] + b
+        if y_at_bot_left >= self.bot_left[0]:
+            intersect_y = slope * self.bot_right[0] + b
+            target = np.array([self.bot_right[0], intersect_y])
+        else:
+            intersect_x = (self.top_right[1] - b) / slope
+            target = np.array([self.top_right[1], intersect_x])
+        return target