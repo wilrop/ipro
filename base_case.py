bc709c83ca312f8e983805813089746e2cb1157d
Willem Röpke <willem.ropke@vub.be>
2023-02-02 15:05:14 +0100

    rewrite outer loop and implement basic problem setting with inner loop solver

---
+++ base_case.py
@@ -0,0 +1,81 @@
+import argparse
+
+import numpy as np
+
+from geohunt import outer_loop
+from pareto import p_prune
+from vector_u import vector_u_strict
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--objectives', type=int, default=2)
+    parser.add_argument('--vecs', type=int, default=10)
+    parser.add_argument('--low', type=int, default=0)
+    parser.add_argument('--high', type=int, default=10)
+    parser.add_argument('--seed', type=int, default=3)
+    parser.add_argument('--log_dir', type=str, default='runs')
+    parser.add_argument('--save_figs', default=True, action='store_true')
+    return parser.parse_args()
+
+
+def linear_solver(problem, weights):
+    """Find the utility maximising point.
+
+    Args:
+        problem (np.ndarray): The problem to solve.
+        weights (np.ndarray): The weights to use.
+
+    Returns:
+        np.ndarray: The utility maximising point.
+    """
+    utilities = problem @ weights
+    max_utility = np.max(utilities)
+    best_vecs = problem[utilities == max_utility]
+    best_vec = np.array(p_prune({tuple(vec) for vec in best_vecs}).pop())
+    return best_vec
+
+
+def generate_problem(objectives=2, vecs=10, low=0, high=10, rng=None):
+    """Generate a random problem."""
+    if rng is None:
+        rng = np.random.default_rng()
+    return rng.integers(low=low, high=high, size=(vecs, objectives))
+
+
+def inner_loop(problem, target):
+    """The inner loop solver for the basic setting.
+
+    Args:
+        problem (np.ndarray): The problem to solve.
+        target (np.ndarray): The target vector.
+
+    Returns:
+        np.ndarray: The Pareto optimal vector.
+    """
+    best_vec = np.zeros(problem.shape[1])
+    best_u = -np.inf
+    for vec in problem:
+        u = vector_u_strict(vec, target)
+        if u > best_u:
+            best_u = u
+            best_vec = vec
+    return best_vec
+
+
+def verify(pf, problem):
+    pf = {tuple(vec) for vec in pf}
+    candidates = {tuple(vec) for vec in problem}
+    print(f'Candidates: {candidates}')
+    print(f'Pareto front: {pf}')
+    correct_pf = p_prune(candidates)
+    print(f'Correct Pareto front: {correct_pf}')
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    rng = np.random.default_rng(args.seed)
+    problem = generate_problem(objectives=args.objectives, vecs=args.vecs, low=args.low, high=args.high, rng=rng)
+    pf = outer_loop(problem, inner_loop, linear_solver, num_objectives=args.objectives, save_figs=args.save_figs,
+                    log_dir=args.log_dir)
+    verify(pf, problem)
--- geohunt.py
+++ geohunt.py
@@ -1,31 +1,52 @@
 import numpy as np
 from sortedcontainers import SortedKeyList
 
+from pareto import pareto_dominates
 from patch2d import Patch2d
+from vis import plot_patches, create_gif
 
 
-def inner_loop(momdp, target):
-    vec = target
-    return vec
+def outer_loop(problem, inner_loop, linear_solver, num_objectives=2, tolerance=1e-6, save_figs=False, log_dir=None):
+    """Implement the outer loop for the algorithm.
 
+    Args:
+        problem (Problem): A problem instance.
+        inner_loop (function): The inner loop function.
+        linear_solver (function): The linear solver function for the extremities.
+        num_objectives (int, optional): The number of objectives. Defaults to 2.
+        tolerance (float, optional): The tolerance for outer loop to exit. Defaults to 1e-6.
+        save_figs (bool, optional): Whether to save the figures. Defaults to False.
+        log_dir: The directory to save the figures. Defaults to None.
 
-def get_edge_points():
-    return np.array([5, 1]), np.array([1, 5])
+    Returns:
+        list: The Pareto front.
+    """
+    pf = []
 
+    for obj in range(num_objectives):
+        weights = np.zeros(num_objectives)
+        weights[obj] = 1
+        point = linear_solver(problem, weights)
+        pf.append(point)
 
-def geohunt2d(momdp, tolerance=0.1):
-    max_x, max_y = get_edge_points()
-    point1 = np.array([max_y[0], max_x[1]])  # Bottom left intersection of two dominated areas.
-    point2 = np.array([max_x[0], max_y[1]])  # Upper right corner.
-    start_patch = Patch2d(point1, point2)
+    if pareto_dominates(pf[0], pf[1]):
+        pf = [pf[0], pf[0]]
+    elif pareto_dominates(pf[1], pf[0]):
+        pf = [pf[1], pf[1]]
+
+    start_patch = Patch2d(*pf)
     patches_queue = SortedKeyList([start_patch], key=lambda x: x.area)
+    step = 0
 
     while patches_queue:
+        if save_figs:
+            plot_patches(patches_queue, pf, problem, log_dir, f'patches_{step}')
         patch = patches_queue.pop()
 
         target = patch.get_intersection_point()
-        pareto_optimal_vec = inner_loop(momdp, target)
-        if pareto_optimal_vec not in [patch.point1, patch.point2]:  # Check that a new Pareto optimal point was found.
+        pareto_optimal_vec = inner_loop(problem, target)
+        if not patch.on_rectangle(pareto_optimal_vec):  # Check that a new Pareto optimal point was found.
+            pf.append(pareto_optimal_vec)
             new_patch1, new_patch2 = patch.split(pareto_optimal_vec)
 
             if new_patch1.area > tolerance:
@@ -34,8 +55,10 @@ def geohunt2d(momdp, tolerance=0.1):
             if new_patch2.area > tolerance:
                 patches_queue.add(new_patch2)
 
+        step += 1
 
-if __name__ == '__main__':
-    momdp = 0
-    tolerance = 0.1
-    geohunt2d(momdp, tolerance=tolerance)
+    if save_figs:
+        plot_patches(patches_queue, pf, problem, log_dir, f'patches_{step}')
+        create_gif(log_dir, 'algorithm')
+
+    return pf
