fea1f0b87dd3121a14bd2bde0f4890694eb247fa
Willem Röpke <willem.ropke@vub.be>
2023-02-02 14:32:13 +0100

    Pareto functionality for verification

---
+++ pareto.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+
+def pareto_dominates(a, b):
+    """Check if the vector in a Pareto dominates vector b.
+
+    Args:
+        a (ndarray): A numpy array.
+        b (ndarray): A numpy array.
+
+    Returns:
+        bool: Whether vector a dominates vector b.
+    """
+    a = np.array(a)
+    b = np.array(b)
+    return np.all(a >= b) and np.any(a > b)
+
+
+def p_prune(candidates):
+    """Create a Pareto coverage set from a set of candidate points.
+
+    References:
+        .. [1] Roijers, D. M., & Whiteson, S. (2017). Multi-objective decision making. 34, 129–129.
+            https://doi.org/10.2200/S00765ED1V01Y201704AIM034
+
+    Args:
+        candidates (Set[Tuple]): A set of vectors.
+
+    Returns:
+        Set[Tuple]: A Pareto coverage set.
+    """
+    pcs = set()
+    while candidates:
+        vector = candidates.pop()
+
+        for alternative in candidates:
+            if pareto_dominates(alternative, vector):
+                vector = alternative
+
+        to_remove = set(vector)
+        for alternative in candidates:
+            if pareto_dominates(vector, alternative):
+                to_remove.add(alternative)
+
+        candidates -= to_remove
+        pcs.add(vector)
+    return pcs
