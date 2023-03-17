bc222c565208e47706a3efd7a43f1cf05dce63cd
Willem Röpke <willem.ropke@vub.be>
2023-03-17 08:01:05 +0100

    Add test to empirically evaluate whether the utility function is concave

--- tests.py
+++ tests.py
@@ -1,7 +1,8 @@
 import numpy as np
 
-import vector_u
 import maximum_empty_box as meb
+import vector_u
+
 
 def test_func_on_vectors(target, nadir, vectors, func):
     for vector in vectors:
@@ -20,7 +21,7 @@ def test_utility_funcs():
     print(f'Nadir: {nadir}')
     print(f'Ideal: {ideal}')
 
-    utility_functions = [vector_u.rectangle_u, vector_u.fast_rectangle_u ,vector_u.fast_translated_rectangle_u]
+    utility_functions = [vector_u.rectangle_u, vector_u.fast_rectangle_u, vector_u.fast_translated_rectangle_u]
     vectors = [vector1, vector2, vector3, vector4]
 
     for func in utility_functions:
@@ -40,6 +41,28 @@ def test_maximum_emtpy_box():
     print(f'New Rectangle: {new_nadir} - {new_ideal}')
 
 
+def test_concavity():
+    tries = 1000000
+    num_dims = 2
+    low = -5
+    high = 5
+    func = vector_u.fast_translated_rectangle_u
+    for i in range(tries):
+        nadir = np.random.uniform(low=low, high=high, size=num_dims)
+        ideal = np.random.uniform(low=low, high=high, size=num_dims)
+        vec1 = np.random.uniform(low=low, high=high, size=num_dims)
+        vec2 = np.random.uniform(low=low, high=high, size=num_dims)
+        alpha = np.random.uniform()
+        out1 = func((1 - alpha) * vec1 + alpha * vec2, ideal, nadir)
+        out2 = (1 - alpha) * func(vec1, ideal, nadir) + alpha * func(vec2, ideal, nadir)
+        if not out1 >= out2 and not np.isclose(out1, out2):
+            print(out1, out2)
+            raise ValueError('Not concave.')
+
+    print('Empirically verified that the utility function is concave.')
+
+
 if __name__ == '__main__':
     test_utility_funcs()
     test_maximum_emtpy_box()
+    test_concavity()
