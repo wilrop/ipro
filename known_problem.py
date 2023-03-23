aaf5ead99b85d50da7697dc8a75d6a710ba27208
Willem Röpke <willem.ropke@vub.be>
2023-03-23 17:10:35 +0100

    Execute known problems for multiple iterations

--- known_problem.py
+++ known_problem.py
@@ -10,12 +10,13 @@ from vector_u import create_batched_fast_translated_rectangle_u
 
 def parse_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--iterations', type=int, default=100, help='The number of iterations.')
     parser.add_argument('--objectives', type=int, default=3, help='The number of objectives.')
     parser.add_argument('--vecs', type=int, default=10, help='The number of vectors.')
     parser.add_argument('--low', type=int, default=0, help='The lower bound for the random integers.')
     parser.add_argument('--high', type=int, default=10, help='The upper bound for the random integers.')
     parser.add_argument('--outer_loop', type=str, default='ND', help='The outer loop to use.')
-    parser.add_argument('--seed', type=int, default=6, help='The seed for the random number generator.')
+    parser.add_argument('--seed', type=int, default=1, help='The seed for the random number generator.')
     return parser.parse_args()
 
 
@@ -76,26 +77,33 @@ class InnerLoop:
 if __name__ == '__main__':
     args = parse_args()
     rng = np.random.default_rng(args.seed)
-    problem = generate_problem(objectives=args.objectives, vecs=args.vecs, low=args.low, high=args.high, rng=rng)
-    inner_loop = InnerLoop(problem)
 
-    if args.outer_loop == '2D':
-        if args.objectives != 2:
-            raise ValueError('The 2D outer loop can only be used for 2D problems.')
-        outer_loop = OuterLoop2D(problem, inner_loop, linear_solver)
-    else:
-        outer_loop = OuterLoop(problem, args.objectives, inner_loop, linear_solver, seed=args.seed)
+    for i in range(args.iterations):
+        print(f'Iteration {i}')
 
-    pf = outer_loop.solve()
-    correct_set, is_correct = verify_pcs(problem, pf)
-    missing_set = correct_set - pf
-    print(f'Is correct: {is_correct}')
-    print(f'Correct set: {correct_set}')
-    print(f'Obtained set: {pf}')
-    print(f'Number of missing elements: {len(missing_set)} - Difference : {missing_set}')
-    print(f'Bounding box: {outer_loop.bounding_box}')
+        problem = generate_problem(objectives=args.objectives, vecs=args.vecs, low=args.low, high=args.high, rng=rng)
+        inner_loop = InnerLoop(problem)
 
-    for point in missing_set:
-        for box in outer_loop.removed_boxes:
-            if box.contains(point):
-                print(f'Point {point} was removed by box {box}. Inner point? {box.contains_inner(point)}')
+        if args.outer_loop == '2D':
+            if args.objectives != 2:
+                raise ValueError('The 2D outer loop can only be used for 2D problems.')
+            outer_loop = OuterLoop2D(problem, inner_loop, linear_solver)
+        else:
+            outer_loop = OuterLoop(problem, args.objectives, inner_loop, linear_solver, seed=args.seed)
+
+        pf = outer_loop.solve()
+        correct_set, is_correct = verify_pcs(problem, pf)
+        missing_set = correct_set - pf
+
+        if not is_correct:
+            print(f'Problem: {problem}')
+            print(f'Is correct: {is_correct}')
+            print(f'Correct set: {correct_set}')
+            print(f'Obtained set: {pf}')
+            print(f'Number of missing elements: {len(missing_set)} - Difference : {missing_set}')
+            print(f'Bounding box: {outer_loop.bounding_box}')
+
+            for point in missing_set:
+                for box in outer_loop.removed_boxes:
+                    if box.contains(point):
+                        print(f'Point {point} was removed by box {box}. Inner point? {box.contains_inner(point)}')
