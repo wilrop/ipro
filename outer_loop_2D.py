819293cd23e1858eaf04644d29f39bae61d3a652
Willem Röpke <willem.ropke@vub.be>
2023-03-28 12:44:36 +0200

    Updates to 2D loop making it fit with the OL API

--- outer_loop_2D.py
+++ outer_loop_2D.py
@@ -16,13 +16,36 @@ class OuterLoop2D:
         self.log_dir = log_dir
 
         self.bounding_box = None
+        self.removed_boxes = []
         self.box_queue = SortedKeyList([], key=lambda x: x.volume)
         self.pf = set()
+
+        self.covered_volume = 0
 
     def reset(self):
         """Reset the algorithm to its initial state."""
         self.bounding_box = None
+        self.removed_boxes = []
         self.box_queue = SortedKeyList([], key=lambda x: x.volume)
+        self.pf = set()
+        self.covered_volume = 0
+
+    def percentage_covered(self):
+        """Get the percentage of the bounding box that is covered.
+
+        Returns:
+            float: The percentage of the bounding box that is covered.
+        """
+        return self.covered_volume / self.bounding_box.volume * 100
+
+    def remove_box(self, box):
+        """Remove a box from the algorithm.
+
+        Args:
+            box (Box): The box to remove.
+        """
+        self.removed_boxes.append(box)
+        self.covered_volume += box.volume
 
     def split_box(self, box, point):
         """Split a box into two new boxes.
@@ -41,6 +64,9 @@ class OuterLoop2D:
         nadir2 = np.array([point[0], box.nadir[1]])
         ideal2 = np.array([box.ideal[0], point[1]])
         new_box2 = Box(ideal2, nadir2)
+
+        self.remove_box(Box(box.nadir, point))
+        self.remove_box(Box(point, box.ideal))
         return new_box1, new_box2
 
     def update(self, box, point):
@@ -83,7 +109,7 @@ class OuterLoop2D:
     def get_next_box(self):
         """Get the next box to search."""
         if self.box_queue:
-            return self.box_queue.pop(0)
+            return self.box_queue.pop(-1)
         else:
             raise Exception('No more boxes to split.')
 
@@ -91,35 +117,47 @@ class OuterLoop2D:
         """Check if the algorithm is done."""
         return not self.box_queue
 
-    def accept_point(self, point):
+    def accept_point(self, point, utility, fast=True):
         """Check if a point is valid.
+
+        Note:
+            Preferable, this should use the utility but it does not work yet.
 
         Args:
             point (np.array): The point to check.
+            utility (float): The utility of the point.
 
         Returns:
             bool: True if the point is valid, False otherwise.
         """
+        if fast:
+            return utility > 0
+
         for alternative in self.pf:
             if pareto_dominates(alternative, point) or np.all(alternative == point):
                 return False
         return True
 
-    def solve(self):
+    def solve(self, log_freq=10):
         """Solve the problem."""
         self.init_phase()
         step = 0
 
         while not self.is_done():
+            if step % log_freq == 0:
+                print(f'Step {step} - Covered volume: {self.percentage_covered():.5f}%')
+
             box = self.get_next_box()
-            target = box.ideal
-            local_nadir = box.nadir
+            target = np.copy(box.ideal)
+            local_nadir = np.copy(box.nadir)
             found_vec, utility = self.inner_loop.solve(target, local_nadir)
 
-            if self.accept_point(found_vec):  # Check that new point is valid.
+            if self.accept_point(found_vec, utility):  # Check that new point is valid.
                 self.update(box, found_vec)
+            else:
+                self.remove_box(box)
 
             step += 1
 
-        pf = p_prune({tuple(vec) for vec in self.pf})
+        pf = p_prune(self.pf.copy())
         return pf
