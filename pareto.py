400d58b85e6983d54cad1bd28275f2abba1dfb30
Willem Röpke <willem.ropke@vub.be>
2023-04-25 12:23:51 +0200

    Add fast pruning and pareto dominance operators

--- pareto.py
+++ pareto.py
@@ -16,6 +16,53 @@ def pareto_dominates(a, b):
     return np.all(a >= b) and np.any(a > b)
 
 
+def strict_pareto_dominates(a, b):
+    """Check if the vector in a Pareto strictly dominates vector b.
+
+    Args:
+        a (ndarray): A numpy array.
+        b (ndarray): A numpy array.
+
+    Returns:
+        bool: Whether vector a strictly dominates vector b.
+    """
+    a = np.array(a)
+    b = np.array(b)
+    return np.all(a > b)
+
+
+def weak_pareto_dominates(a, b):
+    """Check if the vector in a Pareto weakly dominates vector b.
+
+    Args:
+        a (ndarray): A numpy array.
+        b (ndarray): A numpy array.
+
+    Returns:
+        bool: Whether vector a weakly dominates vector b.
+    """
+    a = np.array(a)
+    b = np.array(b)
+    return np.any(a > b)
+
+
+def extreme_prune(candidates):
+    """A batched and fast version of the Pareto coverage set algorithm.
+
+    Args:
+        candidates (ndarray): A numpy array of vectors.
+
+    Returns:
+        ndarray: A Pareto coverage set.
+    """
+    candidates = np.unique(candidates, axis=0)
+    res_eq = np.all(candidates[:, None, None] <= candidates, axis=-1).squeeze()
+    res_g = np.all(candidates[:, None, None] < candidates, axis=-1).squeeze()
+    c1 = np.sum(res_eq, axis=-1) == 1
+    c2 = np.any(~res_g, axis=-1)
+    return candidates[np.logical_and(c1, c2)]
+
+
 def p_prune(candidates):
     """Create a Pareto coverage set from a set of candidate points.
 
