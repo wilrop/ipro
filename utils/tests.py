89ef60878ec2695d5fbfa305b07441ea1bf73461
Willem Röpke <willem.ropke@vub.be>
2023-06-29 18:45:19 +0200

    Extend tests

--- utils/tests.py
+++ utils/tests.py
@@ -11,20 +11,22 @@ def check_utility():
 
 
 def rank_points():
-    ref = np.array([0, -19])
-    ideal = np.array([124, 0])
+    refs = np.array([[0, -19], [0., -14.]])
+    ideals = np.array([[124, 0], [50., 0.]])
     vecs = [np.array([0, 0]), np.array([1, -1]), np.array([2, -3]), np.array([3, -5]), np.array([5, -7]),
             np.array([8, -8]),
             np.array([16, -9]), np.array([24, -13]), np.array([50, -14]), np.array([74, -17]), np.array([124, -19])]
     augs = [0, 0.005, 0.01, 0.1]
+    scale = 1000
 
     for aug in augs:
-        res = [(vec, aasf(vec, ref, ref, ideal, aug=aug, backend='numpy')) for vec in vecs]
-        order = sorted(res, key=lambda x: x[1], reverse=True)
+        for ref, ideal in zip(refs, ideals):
+            res = [(vec, aasf(vec, ref, ref, ideal, aug=aug, scale=scale, backend='numpy')) for vec in vecs]
+            order = sorted(res, key=lambda x: x[1], reverse=True)
 
-        print(f'Augmentation: {aug}')
-        for i, (vec, val) in enumerate(order):
-            print(f'{i + 1}. {vec} -> {val}')
+            print(f'Augmentation: {aug}')
+            for i, (vec, val) in enumerate(order):
+                print(f'{i + 1}. {ref} - {ideal}: {vec} -> {val}')
 
 
 if __name__ == '__main__':
