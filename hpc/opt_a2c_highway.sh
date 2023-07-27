bb390810ffc79748cca7e9e4964a36a8e02d5b3f
Willem Röpke <willem.ropke@vub.be>
2023-07-27 17:46:03 +0200

    Add jobscripts for the cluster

---
+++ hpc/opt_a2c_highway.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH --job-name=opt_a2c_highway
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem=8gb
+#SBATCH --mail-user=willem.ropke@vub.be
+#SBATCH --mail-type=ALL
+#SBATCH --output=logs/output-%A.out
+#SBATCH --error=logs/err-%A.err
+
+# Load the necessary modules.
+module load Python/3.10.4-GCCcore-11.3.0
+module load SciPy-bundle/2022.05-foss-2022a
+module load PyTorch/1.12.0-foss-2022a
+pip install --user gymnasium
+pip install --user mo-gymnasium
+pip install --user highway-env
+pip install --user tensorboard
+pip install --user wandb
+pip install --user pygmo
+
+# Define the log directory.
+LOGDIR="${VSC_SCRATCH}/IPRO"
+
+# Run the experiments.
+python3 $VSC_HOME/geohunt/experiments/opt_a2c_highway.py \
+  --log-dir "$LOGDIR"
