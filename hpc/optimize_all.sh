9eaf5cc17a42fd1733ecfb71de9a4ea5e7dc03a9
Willem Röpke <willem.ropke@vub.be>
2023-08-11 14:20:18 +0200

    Load new optuna file and sleep a random number of seconds before starting a new job

--- hpc/optimize_all.sh
+++ hpc/optimize_all.sh
@@ -15,7 +15,7 @@ module load Python/3.10.4-GCCcore-11.3.0
 module load SciPy-bundle/2022.05-foss-2022a
 module load PyTorch/1.12.0-foss-2022a
 module load pygmo/2.18.0-foss-2022a
-module load Optuna/2.10.0-foss-2021b
+module load Optuna/3.1.0-foss-2022a
 module load tensorboard/2.10.0-foss-2022a
 module load wandb/0.13.4-GCCcore-11.3.0
 
@@ -36,6 +36,9 @@ export PYTHONPATH="${PYTHONPATH}:$VSC_HOME/geohunt"
 
 # Set wandb directory.
 export WANDB_DIR=$VSC_SCRATCH
+
+# Sleep for a random number of seconds to avoid overloading the file system.
+sleep $[ ( $RANDOM % 20 )  + 1 ]s
 
 # Run the experiments.
 python3 ${OPTIMIZATION_DIR}/search.py \
