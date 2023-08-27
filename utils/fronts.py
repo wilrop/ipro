288dfe2b5f5343371858aad921c4db03685237b2
Willem Röpke <willem.ropke@vub.be>
2023-08-27 14:17:48 +0300

    Separate extraction of fronts from JSON to use for other experiments as well

--- utils/fronts.py
+++ utils/fronts.py
@@ -3,7 +3,28 @@ import numpy as np
 import pygmo as pg
 
 
+def extract_front_from_json(file_name):
+    """Extract the Pareto front from a file name.
+
+    Args:
+        file_name (str): The file to load the results from.
+
+    Returns:
+        Dict: A dictionary with the point found for each iteration.
+    """
+    with open(file_name, "r") as f:
+        data = json.load(f)
+
+    front = {}
+    for key, value in data.items():
+        if key.startswith("pareto_point"):
+            iter_point = int(key.split('_')[-1])
+            front[iter_point] = value
+    return front
+
+
 def minecart_fronts():
+    """Compute the Pareto fronts for minecart."""
     ideals = np.array([[1.5, 0., -0.95999986], [0., 1.5, -0.95999986], [0., 0., -0.24923698]])
     correct_vecs = np.array(
         [np.array([0.55612687, 0.11092755, -0.65061296]), np.array([0.85192097, 0.16992796, -0.81650404]),
@@ -17,18 +38,9 @@ def minecart_fronts():
          np.array([0.16992796, 0.85192097, -0.81650404]), np.array([0.12026385, 0.60293373, -0.77743027]),
          np.array([0.18423008, 0.92362357, -0.94917702]), np.array([0., 0., -0.24923698])])
 
-    computed_vecs = []
-
     nadir = np.array([0., 0., -3.1199985]) - 1
-
-    with open("minecart.json", "r") as f:
-        data = json.load(f)
-
-    for key, value in data.items():
-        if key.startswith("pareto_point"):
-            computed_vecs.append(value)
-    computed_vecs = np.array(computed_vecs)
-
+    computed_front = extract_front_from_json("minecart.json")
+    computed_vecs = np.array(list(computed_front.values()))
     correct_vecs = np.vstack((correct_vecs, ideals))
     computed_vecs = np.vstack((computed_vecs, ideals))
 
@@ -42,6 +54,7 @@ def minecart_fronts():
 
 
 def dst_fronts():
+    """Compute the Pareto fronts for DST."""
     vecs = np.array([[0.0, 0.0], [1.0, -1.0], [2.0, -3.0], [3.0, -5.0], [5.0, -7.0], [8.0, -8.0], [16.0, -9.0],
                      [24.0, -13.0], [50.0, -14.0], [74.0, -17.0], [124.0, -19.0]])
     nadir = np.array([0, -19.0]) - 1
