419ccc5312394ab821d328cac4c2ccb4aafb4a2f
Willem Röpke <willem.ropke@vub.be>
2023-09-06 16:23:52 +0200

    Allocate a bit less jobs actually

--- hpc/optimize_all.sh
+++ hpc/optimize_all.sh
@@ -8,7 +8,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --output=logs/output-%A.out
 #SBATCH --error=logs/err-%A.err
-#SBATCH --array=1-200
+#SBATCH --array=1-150
 
 # Load the necessary modules.
 module load Python/3.10.4-GCCcore-11.3.0
