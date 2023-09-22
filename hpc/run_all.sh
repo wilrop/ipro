9ff65674d66428cdf85c36ac4b395228a94841c4
Willem Röpke <willem.ropke@vub.be>
2023-09-22 09:24:11 +0200

    Run all experiments

---
+++ hpc/run_all.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#SBATCH --job-name=optimize
+#SBATCH --time=48:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem=2gb
+#SBATCH --mail-user=willem.ropke@vub.be
+#SBATCH --mail-type=ALL
+#SBATCH --array=1-135
+
+# Load the necessary modules.
+module load Python/3.10.4-GCCcore-11.3.0
+module load SciPy-bundle/2022.05-foss-2022a
+module load PyTorch/1.12.0-foss-2022a
+module load pygmo/2.18.0-foss-2022a
+module load Optuna/3.1.0-foss-2022a
+module load tensorboard/2.10.0-foss-2022a
+module load wandb/0.13.4-GCCcore-11.3.0
+module load MuJoCo/2.2.2-GCCcore-11.3.0
+
+pip install --user gymnasium
+pip install --user mo-gymnasium
+pip install --user highway-env
+
+export OMP_NUM_THREADS=1
+
+# Define variables.
+EXPERIMENT_DIR="${VSC_HOME}/geohunt/experiments"
+
+# Set pythonpath
+export PYTHONPATH="${PYTHONPATH}:$VSC_HOME/geohunt"
+
+# Set wandb directory.
+export WANDB_DIR=$VSC_SCRATCH
+
+# Sleep for a random number of seconds to avoid overloading the file system.
+sleep $((($RANDOM % 20) + 1))s
+
+# Run the experiments.
+python3 ${EXPERIMENT_DIR}/run_experiments.py \
+  --exp_id ${SLURM_ARRAY_TASK_ID}
