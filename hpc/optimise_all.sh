633578cc624fd18bedf2986810216caaf81d5185
Willem Röpke <willem.ropke@vub.be>
2023-10-24 10:13:12 +0200

    Update DQN HP search

--- hpc/optimise_all.sh
+++ hpc/optimise_all.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #SBATCH --job-name=optimise
-#SBATCH --time=48:00:00
+#SBATCH --time=24:00:00
 #SBATCH --ntasks=1
 #SBATCH --mem=2gb
 #SBATCH --mail-user=willem.ropke@vub.be
 #SBATCH --mail-type=ALL
-#SBATCH --array=1-100
+#SBATCH --array=1-200
 
 # Load the necessary modules.
 module load Python/3.10.4-GCCcore-11.3.0
