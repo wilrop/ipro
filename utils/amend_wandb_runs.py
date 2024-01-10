4912b76dcb3e4f461c639e042075a7d61caff7f8
Willem Röpke <willem.ropke@vub.be>
2024-01-10 14:57:31 +0100

    Script to amend wandb histories for easier processing

---
+++ utils/amend_wandb_runs.py
@@ -0,0 +1,21 @@
+import wandb
+
+
+def amend_wandb_runs():
+    """Amend the hypervolume of runs."""
+    api = wandb.Api(timeout=120)
+    runs = api.runs("wilrop/IPRO_opt")
+    for run in runs:
+        if 'outer/hypervolume' not in run.summary:
+            try:
+                run.summary['outer/hypervolume'] = run.history(keys=['outer/hypervolume']).iloc[-1]['outer/hypervolume']
+            except IndexError:
+                run.summary['outer/hypervolume'] = 0.0
+            run.summary.update()
+            print(f'❌ Amended {run.name}')
+        else:
+            print(f'✔️ Already amended {run.name}')
+
+
+if __name__ == '__main__':
+    amend_wandb_runs()
