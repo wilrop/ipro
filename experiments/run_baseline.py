948d9e26deb05733cd6a08e337c13af0bd13981d
Willem Röpke <willem.ropke@vub.be>
2024-01-21 14:14:27 +0100

    Fix bug

--- experiments/run_baseline.py
+++ experiments/run_baseline.py
@@ -159,8 +159,8 @@ def run_baseline(exp_id, exp_dir):
         one_hot = True
     else:
         one_hot = False
-    env = setup_env(env_id, gamma=gamma, max_episode_steps=max_episode_steps, one_hot=one_hot)
-    eval_env = setup_env(env_id, gamma=gamma, max_episode_steps=max_episode_steps, one_hot=one_hot)
+    env, _ = setup_env(env_id, gamma=gamma, max_episode_steps=max_episode_steps, one_hot=one_hot)
+    eval_env, _ = setup_env(env_id, gamma=gamma, max_episode_steps=max_episode_steps, one_hot=one_hot)
 
     agent = setup_agent(baseline, env, gamma, seed, setup_kwargs)
     agent.train(total_timesteps, eval_env, ref_point, **train_kwargs)
