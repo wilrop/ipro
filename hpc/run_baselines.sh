d775a35bb88e1139d96773b053acc134bea956b6
Willem Röpke <willem.ropke@vub.be>
2024-01-24 20:42:33 +0100

    Rerun PCN experiments with fixed code

--- hpc/run_baselines.sh
+++ hpc/run_baselines.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #SBATCH --job-name=run_baselines
-#SBATCH --time=24:00:00
+#SBATCH --time=120:00:00
 #SBATCH --ntasks=1
 #SBATCH --mem=3gb
 #SBATCH --mail-user=willem.ropke@vub.be
 #SBATCH --mail-type=ALL
-#SBATCH --array=1-5
+#SBATCH --array=1-10
 
 # Load the necessary modules.
 module load Python/3.10.4-GCCcore-11.3.0
