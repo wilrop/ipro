483d3b4545cfe5ed81b2e87afeb7835aa9d2c2f7
Willem Röpke <willem.ropke@vub.be>
2024-01-24 17:46:27 +0100

    Fix script

--- hpc/run_leftovers.sh
+++ hpc/run_leftovers.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#SBATCH --job-name=run_baselines
-#SBATCH --time=24:00:00
+#SBATCH --job-name=run_leftovers
+#SBATCH --time=5:00:00
 #SBATCH --ntasks=1
-#SBATCH --mem=3gb
+#SBATCH --mem=2gb
 #SBATCH --mail-user=willem.ropke@vub.be
 #SBATCH --mail-type=ALL
 #SBATCH --array=1-9
@@ -13,6 +13,7 @@ module load Python/3.10.4-GCCcore-11.3.0
 module load SciPy-bundle/2022.05-foss-2022a
 module load PyTorch/1.12.0-foss-2022a
 module load pygmo/2.18.0-foss-2022a
+module load Optuna/3.1.0-foss-2022a
 module load wandb/0.13.4-GCCcore-11.3.0
 module load MuJoCo/2.2.2-GCCcore-11.3.0
 
@@ -28,14 +29,11 @@ export PYTHONPATH="${PYTHONPATH}:$VSC_HOME/ipro"
 # Set wandb directory.
 export WANDB_DIR=$VSC_SCRATCH
 
-# Set wandb in offline mode.
-wandb offline
-
 # Sleep for a random number of seconds to avoid overloading the file system.
 sleep $((($RANDOM % 20) + 1))s
 
 # Run the experiments.
-python3 ${EXPERIMENT_DIR}/run_baseline.py \
+python3 ${EXPERIMENT_DIR}/reproduce_experiment.py \
   --exp_id ${SLURM_ARRAY_TASK_ID} \
   --exp_dir ${EXPERIMENT_DIR}/${JSON_DIR} \
   --leftovers
