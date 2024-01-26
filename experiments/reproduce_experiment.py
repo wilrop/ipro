06fe811ede3bd0de8c2975f1e3530deba72d8127
Willem Röpke <willem.ropke@vub.be>
2024-01-26 18:05:34 +0100

    Run grid experiments

--- experiments/reproduce_experiment.py
+++ experiments/reproduce_experiment.py
@@ -64,16 +64,19 @@ def setup_outer_params(tolerance):
     return outer_params
 
 
-def setup_oracle_params(parameters):
+def setup_oracle_params(parameters, grid=False):
     """Setup the parameters for the oracle."""
     oracle_params = {
         **parameters,
         'track': False
     }
+    if grid:
+        oracle_params['grid_sample'] = True
+        oracle_params['pretrain_iters'] = 25
     return oracle_params
 
 
-def reproduce_experiment(oracle, env_id, seed, run_id):
+def reproduce_experiment(oracle, env_id, seed, run_id, grid=False):
     """Reproduce an experiment."""
     api = wandb.Api(timeout=120)
     run = api.run(f'{run_id}')
@@ -88,20 +91,22 @@ def reproduce_experiment(oracle, env_id, seed, run_id):
     method = parameters.pop('method')
     config = setup_config(parameters, seed, max_episode_steps, one_hot_wrapper)
     outer_params = setup_outer_params(tolerance)
-    oracle_params = setup_oracle_params(parameters)
+    oracle_params = setup_oracle_params(parameters, grid=grid)
 
     # Run experiment and mark as reproduced.
     run_experiment(method, oracle, config, outer_params, oracle_params, extra_config=extra_config)
 
 
-def reproduce_from_id(exp_id, exp_dir, leftovers=False):
+def reproduce_from_id(exp_id, exp_dir, leftovers=False, grid=False):
     """Reproduce an experiment given its ID."""
     if leftovers:
         id_exp_dict = json.load(open(f'{exp_dir}/leftovers.json', 'r'))
+    elif grid:
+        id_exp_dict = json.load(open(f'{exp_dir}/grid_experiments.json', 'r'))
     else:
         id_exp_dict = json.load(open(f'{exp_dir}/experiments.json', 'r'))
     alg, env_id, seed, run_id = id_exp_dict[str(exp_id)]
-    return reproduce_experiment(alg, env_id, seed, run_id)
+    return reproduce_experiment(alg, env_id, seed, run_id, grid=grid)
 
 
 if __name__ == '__main__':
@@ -109,6 +114,7 @@ if __name__ == '__main__':
     parser.add_argument('--exp_id', type=str, default=1)
     parser.add_argument('--exp_dir', type=str, default='./evaluation')
     parser.add_argument('--leftovers', default=False, action='store_true')
+    parser.add_argument('--grid', default=False, action='store_true')
     args = parser.parse_args()
 
-    reproduce_from_id(args.exp_id, args.exp_dir, args.leftovers)
+    reproduce_from_id(args.exp_id, args.exp_dir, args.leftovers, args.grid)
