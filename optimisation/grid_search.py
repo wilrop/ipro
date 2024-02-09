f1a17cec9a6ad7376eb80dd8c01bd455b9cee7dc
Willem Röpke <willem.ropke@vub.be>
2024-02-09 23:37:31 +0100

    Fix grid search

--- optimisation/grid_search.py
+++ optimisation/grid_search.py
@@ -19,19 +19,21 @@ def grid_search(parameters, exp_id, offset):
     algorithm = oracle_params.pop('algorithm')
     seeds = parameters.pop('seeds')
 
-    # Determine the parameter combination from the flat id.
-    grid = [(key, v['choices']) for key, v in list(parameters.pop('hyperparameters').items())]
+    # Extract and sort the grid which is useful for logging.
+    grid = list(parameters.pop('hyperparameters').items())
+    grid = [(key, v['choices']) for key, v in grid]
+    grid = sorted(grid, key=lambda x: x[0])
     grid_shape = tuple([len(v) for k, v in grid] + [len(seeds)])
-    idx = np.unravel_index(exp_id + offset - 1, grid_shape)  # Determine the parameter combination from the flat id.
     print(f'Grid needs {np.prod(grid_shape)} experiments.')
 
-    # Create ordered parameter group hash for the experiment.
-    sorted_grid = sorted(grid, key=lambda x: x[0])
-    group_str = '_'.join([f'{key}={values[idx[i]]}' for i, (key, values) in enumerate(sorted_grid)])
+    # Create key values for the experiment.
+    idx = np.unravel_index(exp_id + offset - 1, grid_shape)  # Determine the parameter combination from the flat id.
+    oracle_hyperparams = [(key, values[i]) for (key, values), i in zip(grid, idx)]
+    group_str = '_'.join([f'{k}={v}' for k, v in oracle_hyperparams])
     extra_config = {'group': group_str}
 
-    # Create the oracle hyperparameters and set the seed.
-    oracle_hyperparams = {key: values[idx[i]] for i, (key, values) in enumerate(grid)}
+    # Create the filled oracle hyperparameters and set the seed.
+    oracle_hyperparams = dict(oracle_hyperparams)
     filled_oracle_params = {**oracle_params, **oracle_hyperparams}
     parameters['seed'] = seeds[idx[-1]]
     return run_experiment(method, algorithm, parameters, outer_params, filled_oracle_params, extra_config=extra_config)
