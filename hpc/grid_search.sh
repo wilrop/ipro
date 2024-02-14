18c825af6099cc4f4042b33660cf8c1c2c3e1478
Willem Röpke <willem.ropke@vub.be>
2024-02-14 15:07:14 +0100

    HPs

--- hpc/grid_search.sh
+++ hpc/grid_search.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem=2gb
 #SBATCH --mail-user=willem.ropke@vub.be
 #SBATCH --mail-type=ALL
-#SBATCH --array=1-50
+#SBATCH --array=1-240
 
 # Load the necessary modules.
 module load Python/3.10.4-GCCcore-11.3.0
