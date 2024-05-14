a1678f37c411ecddfde638acff2092a0378e4854
Willem Röpke <willem.ropke@vub.be>
2024-05-14 09:49:11 +0200

    Update jobscript and reproduce script

--- experiments/reproduce_experiment.py
+++ experiments/reproduce_experiment.py
@@ -78,7 +78,7 @@ def setup_oracle_params(parameters):
     return oracle_params
 
 
-def reproduce_experiment(oracle, env_id, seed, run_id):
+def reproduce_experiment(oracle, env_id, seed, run_id, u_dir):
     """Reproduce an experiment."""
     api = wandb.Api(timeout=120)
     run = api.run(f'{run_id}')
@@ -96,24 +96,30 @@ def reproduce_experiment(oracle, env_id, seed, run_id):
     oracle_params = setup_oracle_params(parameters)
 
     # Run experiment and mark as reproduced.
-    run_experiment(method, oracle, config, outer_params, oracle_params, extra_config=extra_config)
+    run_experiment(method, oracle, config, outer_params, oracle_params, u_dir, extra_config=extra_config)
 
 
-def reproduce_from_id(exp_id, exp_dir, leftovers=False):
+def reproduce_from_id(u_dir, exp_id, exp_dir, leftovers=False):
     """Reproduce an experiment given its ID."""
     if leftovers:
         id_exp_dict = json.load(open(f'{exp_dir}/leftovers.json', 'r'))
     else:
         id_exp_dict = json.load(open(f'{exp_dir}/experiments.json', 'r'))
     alg, env_id, seed, run_id = id_exp_dict[str(exp_id)]
-    return reproduce_experiment(alg, env_id, seed, run_id)
+    return reproduce_experiment(alg, env_id, seed, run_id, u_dir)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Reproduce experiments given in a JSON file.')
+    parser.add_argument(
+        '--u_dir',
+        type=str,
+        default='./utility_function/utility_fns',
+        help='Path to directory containing utility functions.'
+    )
     parser.add_argument('--exp_id', type=str, default=1)
     parser.add_argument('--exp_dir', type=str, default='./evaluation')
     parser.add_argument('--leftovers', default=False, action='store_true')
     args = parser.parse_args()
 
-    reproduce_from_id(args.exp_id, args.exp_dir, args.leftovers)
+    reproduce_from_id(args.u_dir, args.exp_id, args.exp_dir, args.leftovers)
--- hpc/run_all.sh
+++ hpc/run_all.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 #SBATCH --job-name=run_all
-#SBATCH --time=5:00:00
+#SBATCH --time=6:00:00
 #SBATCH --ntasks=1
-#SBATCH --mem=2gb
+#SBATCH --mem=4gb
 #SBATCH --mail-user=willem.ropke@vub.be
 #SBATCH --mail-type=ALL
-#SBATCH --array=1-5000
+#SBATCH --array=1-75
 
 # Load the necessary modules.
-module load Python/3.10.4-GCCcore-11.3.0
-module load SciPy-bundle/2022.05-foss-2022a
-module load PyTorch/1.12.0-foss-2022a
-module load pygmo/2.18.0-foss-2022a
-module load Optuna/3.1.0-foss-2022a
-module load wandb/0.13.4-GCCcore-11.3.0
-module load MuJoCo/2.2.2-GCCcore-11.3.0
+module load Python/3.10.4-GCCcore-11.3.0 \
+ SciPy-bundle/2022.05-foss-2022a \
+ PyTorch/1.12.0-foss-2022a \
+ pygmo/2.18.0-foss-2022a \
+ wandb/0.13.4-GCCcore-11.3.0 \
+ MuJoCo/2.2.2-GCCcore-11.3.0
 
 export OMP_NUM_THREADS=1
 
 # Define variables.
 EXPERIMENT_DIR="${VSC_HOME}/ipro/experiments"
 JSON_DIR="evaluation"
+U_DIR="${VSC_HOME}/ipro/utility_function/utility_fns"
 
 # Set pythonpath
 export PYTHONPATH="${PYTHONPATH}:$VSC_HOME/ipro"
@@ -34,5 +34,6 @@ sleep $((($RANDOM % 20) + 1))s
 
 # Run the experiments.
 python3 ${EXPERIMENT_DIR}/reproduce_experiment.py \
+  --u_dir ${U_DIR} \
   --exp_id ${SLURM_ARRAY_TASK_ID} \
   --exp_dir ${EXPERIMENT_DIR}/${JSON_DIR}
