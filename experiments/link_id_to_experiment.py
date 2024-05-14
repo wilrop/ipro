f82d7c14bb3516056b40dd4b6aada8cf543fc5f4
Willem Röpke <willem.ropke@vub.be>
2024-05-14 09:35:22 +0200

    Update experiment linking with given runs. Please give me good results

--- experiments/link_id_to_experiment.py
+++ experiments/link_id_to_experiment.py
@@ -4,7 +4,21 @@ from datetime import datetime
 from itertools import zip_longest
 
 
-def link_id_to_experiment(combos, num_seeds, cutoff_date=None, max_runs_per_config=1000):
+def link_id_to_experiment(to_reproduce):
+    print(f'Linking IDs')
+    idx = 0
+    id_exp = {}
+    for runs in zip_longest(*to_reproduce.values()):
+        for run in runs:
+            if run is not None:
+                idx += 1
+                id_exp[idx] = run
+
+    json.dump(id_exp, open('evaluation/experiments.json', 'w'))
+    print(f'Number of experiments: {idx}')
+
+
+def link_best_runs(combos, num_seeds, cutoff_date=None, max_runs_per_config=1000):
     """Generate a JSON of experiments to reproduce.
 
     Args:
@@ -46,17 +60,18 @@ def link_id_to_experiment(combos, num_seeds, cutoff_date=None, max_runs_per_conf
             reproduce_lst.extend([(alg_name, env_id, seed, run_path) for seed in range(num_seeds)])
         to_reproduce[(env_id, alg_name)] = reproduce_lst
 
-    print(f'Linking IDs')
-    idx = 0
-    id_exp = {}
-    for runs in zip_longest(*to_reproduce.values()):
-        for run in runs:
-            if run is not None:
-                idx += 1
-                id_exp[idx] = run
+    link_id_to_experiment(to_reproduce)
 
-    json.dump(id_exp, open('evaluation/experiments.json', 'w'))
-    print(f'Number of experiments: {idx}')
+
+def link_given_runs(run_paths, num_seeds):
+    to_reproduce = {'all': []}
+    for run_path in run_paths:
+        api = wandb.Api(timeout=120)
+        run = api.run(run_path)
+        alg_name = run.config['alg_name']
+        env_id = run.config['env_id']
+        to_reproduce['all'].extend([(alg_name, env_id, seed, run_path) for seed in range(num_seeds)])
+    link_id_to_experiment(to_reproduce)
 
 
 if __name__ == '__main__':
@@ -67,4 +82,22 @@ if __name__ == '__main__':
     ]
     cutoff_date = "28/01/2024"
     num_seeds = 5
-    link_id_to_experiment(combos, num_seeds, cutoff_date=cutoff_date)
+    #link_best_runs(combos, num_seeds, cutoff_date=cutoff_date)
+    run_paths = [
+        'wilrop/IPRO_dst_neurips/26d3gy5z',
+        'wilrop/IPRO_dst_neurips/uic1iqm2',
+        'wilrop/IPRO_dst_neurips/oiehwksm',
+        'wilrop/IPRO_dst_neurips/34id3l3g',
+        'wilrop/IPRO_dst_neurips/3l26mm8w',
+        'wilrop/IPRO_dst_neurips/l2udoicf',
+        'wilrop/IPRO_dst_neurips/47n4ughs',
+        'wilrop/IPRO_dst_neurips/23okxmc6',
+        'wilrop/IPRO_dst_neurips/3fq1k123',
+        'wilrop/IPRO_dst_neurips/17dk1fpk',
+        'wilrop/IPRO_dst_neurips/nvul5vam',
+        'wilrop/IPRO_dst_neurips/2z7rtg42',
+        'wilrop/IPRO_dst_neurips/2jdq6c2e',
+        'wilrop/IPRO_dst_neurips/2da3ijti',
+        'wilrop/IPRO_dst_neurips/1jooorls'
+    ]
+    link_given_runs(run_paths, num_seeds)
