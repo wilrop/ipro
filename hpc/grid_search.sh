b6aaa8a91b83c5b7ed19ca0b5cb7a4a0f6beca24
Willem Röpke <willem.ropke@vub.be>
2024-05-20 13:05:38 +0200

    Update jobscript

---
+++ hpc/grid_search.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#SBATCH --job-name=gs_a2c_dst
+#SBATCH --time=120:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem=4gb
+#SBATCH --mail-user=willem.ropke@vub.be
+#SBATCH --mail-type=ALL
+#SBATCH --array=1-300
+
+# Load the necessary modules.
+module load Python/3.10.4-GCCcore-11.3.0 \
+  SciPy-bundle/2022.05-foss-2022a \
+  PyTorch/1.12.0-foss-2022a \
+  pygmo/2.18.0-foss-2022a \
+  wandb/0.13.4-GCCcore-11.3.0 \
+  MuJoCo/2.2.2-GCCcore-11.3.0
+
+export OMP_NUM_THREADS=1
+
+# Define variables.
+IPRO_DIR="${VSC_HOME}/ipro"
+OPTIMISATION_DIR="${IPRO_DIR}/optimisation"
+NUM_LINES=$(wc -l <${IPRO_DIR}/hpc/yaml_files.txt)
+LINE=$((${SLURM_ARRAY_TASK_ID} % ${NUM_LINES} + 1))
+SWEEP_ID=$(head -${LINE} ${IPRO_DIR}/hpc/yaml_files.txt | tail -1)
+FN_TYPE="increasing_cumsum"
+U_DIR="${IPRO_DIR}/utility_function/utility_fns/${FN_TYPE}"
+
+# Set pythonpath
+export PYTHONPATH="${PYTHONPATH}:$VSC_HOME/ipro"
+
+# Set wandb directory.
+export WANDB_DIR=$VSC_SCRATCH
+
+# This forces the jobs to start sequentially. The startup time is estimated at 2 seconds.
+sleep $(((${SLURM_ARRAY_TASK_ID} - 1) * 2))s
+
+# Run the experiments.
+python3 ${OPTIMISATION_DIR}/run_agent.py \
+  --project IPRO_dqn_dst_grid \
+  --sweep_id ${SWEEP_ID} \
+  --u_dir ${U_DIR}
