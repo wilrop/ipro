432b45e3dcab8bcaf507bedc771406f3ae173499
Willem Röpke <willem.ropke@vub.be>
2024-05-21 14:12:59 +0200

    Add additional metric and remove unnecessary stack

--- utility_function/utility_eval.py
+++ utility_function/utility_eval.py
@@ -9,10 +9,21 @@ def generalised_maximum_utility_loss(front, reference_set, utility_fns):
         approx_utilities = utility_fn(front)  # Compute the utility for the approximate front
         max_utility_loss = np.max(front_utilities) - np.max(approx_utilities)  # Compute the utility loss.
         utility_losses.append(max_utility_loss)
-    return np.max(np.stack(utility_losses))
+    return np.max(utility_losses)
+
+
+def generalised_expected_utility_loss(front, reference_set, utility_fns):
+    """Compute the expected utility loss for a front and utility functions wrt a reference set."""
+    utility_losses = []
+    for utility_fn in utility_fns:
+        front_utilities = utility_fn(reference_set)  # Compute the utility for the front
+        approx_utilities = utility_fn(front)  # Compute the utility for the approximate front
+        utility_loss = np.max(front_utilities) - np.max(approx_utilities)  # Compute the utility loss.
+        utility_losses.append(utility_loss)
+    return np.mean(utility_losses)
 
 
 def generalised_expected_utility(front, utility_fns):
     """Compute the expected utility for the set of utility functions when taking vectors from the front."""
     utilities = [np.max(utility_fn(front)) for utility_fn in utility_fns]
-    return np.mean(np.stack(utilities))
+    return np.mean(utilities)
