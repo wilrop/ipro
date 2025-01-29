5da63cd9549cd2e50b6944c522228dfd541e968e
Willem Röpke <willem.ropke@vub.be>
2025-01-29 14:07:43 +0100

    Fix typing

--- ipro/linear_solvers/__init__.py
+++ ipro/linear_solvers/__init__.py
@@ -1,8 +1,9 @@
 from ipro.linear_solvers.linear_solver import LinearSolver
 from ipro.linear_solvers.finite import Finite
+from typing import Any
 
 
-def init_linear_solver(alg: str, *args: tuple, **kwargs: dict) -> LinearSolver:
+def init_linear_solver(alg: str, *args: Any, **kwargs: Any) -> LinearSolver:
     """Initialise a linear solver."""
     if alg == 'finite':
         return Finite(*args, **kwargs)
